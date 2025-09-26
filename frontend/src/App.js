9d5b6d0b1f43fc77c56a03d4a68189f6bb9e8464
Ketan Vijay Dhumal <95542980+ketandhumal@users.noreply.github.com>
2025-09-26 16:15:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-#dummy_change 
+//dummy_change 
 import React, { useState, useEffect } from 'react';
 import ProductForm from './components/ProductForm';
 import ProductList from './components/ProductList';
