be5b2b46d494a66c121fe77d30ed93705035e838
ketandhumal <kdhumal914@gmail.com>
2025-11-04 13:06:39 +0530

    db_changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/products', // Replace with our backend URL
+  baseURL: 'https://api.ketansworkproject.space/api/products', // Replace with our backend URL
 });
 
 export default api;
-
